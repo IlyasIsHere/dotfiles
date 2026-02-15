d70248ac75efa9ca8f062bf3957ab3292e00ebd7
IlyasIsHere <ilyashk5@gmail.com>
2026-02-15 22:24:50 +0000

    format misc.lua + add noice.nvim plugin

--- .config/nvim/lua/core/misc.lua
+++ .config/nvim/lua/core/misc.lua
@@ -1,37 +1,37 @@
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-	callback = function()
-		vim.hl.on_yank()
-	end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+    callback = function()
+        vim.hl.on_yank()
+    end,
 })
 
 -- Diagnostic Config
 -- See :help vim.diagnostic.Opts
 vim.diagnostic.config {
-severity_sort = true,
-float = { border = 'rounded', source = 'if_many' },
-underline = { severity = vim.diagnostic.severity.ERROR },
-signs = vim.g.have_nerd_font and {
-  text = {
-    [vim.diagnostic.severity.ERROR] = '󰅚 ',
-    [vim.diagnostic.severity.WARN] = '󰀪 ',
-    [vim.diagnostic.severity.INFO] = '󰋽 ',
-    [vim.diagnostic.severity.HINT] = '󰌶 ',
-  },
-} or {},
-virtual_text = {
-  source = 'if_many',
-  spacing = 2,
-  format = function(diagnostic)
-    local diagnostic_message = {
-      [vim.diagnostic.severity.ERROR] = diagnostic.message,
-      [vim.diagnostic.severity.WARN] = diagnostic.message,
-      [vim.diagnostic.severity.INFO] = diagnostic.message,
-      [vim.diagnostic.severity.HINT] = diagnostic.message,
-    }
-    return diagnostic_message[diagnostic.severity]
-  end,
-},
+    severity_sort = true,
+    float = { border = 'rounded', source = 'if_many' },
+    underline = { severity = vim.diagnostic.severity.ERROR },
+    signs = vim.g.have_nerd_font and {
+        text = {
+            [vim.diagnostic.severity.ERROR] = '󰅚 ',
+            [vim.diagnostic.severity.WARN] = '󰀪 ',
+            [vim.diagnostic.severity.INFO] = '󰋽 ',
+            [vim.diagnostic.severity.HINT] = '󰌶 ',
+        },
+    } or {},
+    virtual_text = {
+        source = 'if_many',
+        spacing = 2,
+        format = function(diagnostic)
+            local diagnostic_message = {
+                [vim.diagnostic.severity.ERROR] = diagnostic.message,
+                [vim.diagnostic.severity.WARN] = diagnostic.message,
+                [vim.diagnostic.severity.INFO] = diagnostic.message,
+                [vim.diagnostic.severity.HINT] = diagnostic.message,
+            }
+            return diagnostic_message[diagnostic.severity]
+        end,
+    },
 }
