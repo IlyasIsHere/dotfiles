01aaf2f18156642cd1b11b0fbaaafa97072929bc
IlyasIsHere <ilyashk5@gmail.com>
2026-02-15 20:08:23 +0000

    Add todo-comments plugin + add keymap to show floating tab for diagnostics

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -24,6 +24,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = '[E]xpand diagnostic message' })
+
 
 -- LSP Keymaps
 vim.api.nvim_create_autocmd('LspAttach', {
