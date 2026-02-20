966e57e6aefc9756e332a38be35a326cb8a034c5
IlyasIsHere <ilyashk5@gmail.com>
2026-02-20 13:38:11 +0000

    Disable insert mode keymaps for toggleterm plugin

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -5,6 +5,8 @@ return {
         opts = {
             open_mapping = [[<leader>t]],
             direction = 'float',
+            insert_mappings = false,   -- whether or not the open mapping applies in insert mode
+            terminal_mappings = false, -- whether or not the open mapping applies in the opened terminals
         }
     }
 }
