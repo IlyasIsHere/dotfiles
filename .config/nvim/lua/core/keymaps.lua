ee2c2bb10c208b85806652cbe96e075cc7190309
IlyasIsHere <ilyashk5@gmail.com>
2026-02-22 16:59:53 +0000

    Add tmux

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -19,11 +19,6 @@ vim.keymap.set("n", "<leader>x", "<Cmd>bdelete<CR>", { desc = "Close buffer" })
 vim.keymap.set("n", "n", "nzzzv", { desc = "Next instance" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous instance" })
 
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = '[E]xpand diagnostic message' })
 
 -- LSP Keymaps
